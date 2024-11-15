84cab5a92db51f5c36b3858c939c850706631308
HoltWeiyangWu <holtwyw@hotmail.com>
2024-11-16 10:47:50 +1100

    Change backend url config var

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = 'http://localhost:8080';
+// export const BACKEND_URL = 'http://localhost:8080';
+
+// JawsDB Add-on from Heroku deployment
+export const BACKEND_URL = process.env.JAWSDB_URL;
